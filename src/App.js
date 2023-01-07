c665a6499fbd1693987293f26a9f433a40b96924
soriya2 <112695380+soriya2@users.noreply.github.com>
2023-01-07 17:14:02 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Calendar from "./components/Calendar";
+
+function App() {
+  return (
+    <div className="App">
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Calendar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Fullcalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+
+function Calendar() {
+  return (
+    <div>
+      <Fullcalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView={"dayGridMonth"}
+        headerToolbar={{
+          start: "today prev,next", // will normally be on the left. if RTL, will be on the right
+          center: "title",
+          end: "dayGridMonth,timeGridWeek,timeGridDay", // will normally be on the right. if RTL, will be on the left
+        }}
+        height={"90vh"}
+      />
+    </div>
+  );
+}
+
+export default Calendar;
